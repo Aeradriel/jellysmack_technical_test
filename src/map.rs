8142c68f17ae6f07eb4afad197cfb5eeb58d002e
Thibaut Roche <thibaut.roche.perso@gmail.com>
2022-02-16 19:13:02 +0100

    [A] Coordinates checks

--- src/map.rs
+++ src/map.rs
@@ -52,4 +52,66 @@ impl Map {
             Ok(())
         }
     }
+
+    pub fn content_coords_for_coords(&self, x: usize, y: usize) -> Result<(usize, usize), String> {
+        if let Some(line) = self.content.get(y) {
+            let mut col_count = 0;
+
+            for (i, c) in line.iter().enumerate() {
+                if c == &'|' {
+                    if col_count == x {
+                        return Ok((i, y));
+                    }
+                    col_count += 1;
+                }
+            }
+            Err("Line has not enough columns for the required coordinates".to_owned())
+        } else {
+            Err("Map has not enough lines for the required coordinates".to_owned())
+        }
+    }
+
+    /// Tells you if you can go to left for the provided coordinates. Coordinates MUST be CONTENT COORDINATES (cf. content_coords_for_coords).
+    /// Using content coordinates allows not to call `Map.content_coords_for_coords` several times.
+    fn can_go_left_for_content_coords(&self, x: usize, y: usize) -> Result<bool, String> {
+        if let Some(line) = self.content.get(y) {
+            // If x is 0 then there is no character on the left
+            // If left char is a '-', then you can go left
+            if x > 0 && line[x - 1] == '-' {
+                Ok(true)
+            } else {
+                Ok(false)
+            }
+        } else {
+            Err("Map has not enough lines for the required coordinates".to_owned())
+        }
+    }
+
+    /// Tells you if you can go to right for the provided coordinates. Coordinates MUST be CONTENT COORDINATES (cf. content_coords_for_coords).
+    /// Using content coordinates allows not to call `Map.content_coords_for_coords` several times.
+    fn can_go_right_for_content_coords(&self, x: usize, y: usize) -> Result<bool, String> {
+        if let Some(line) = self.content.get(y) {
+            // If x is equal to line lenght - 1 then there is no character on the right
+            // If right char is a '-', then you can go left
+            if x < line.len() - 1 && line[x + 1] == '-' {
+                Ok(true)
+            } else {
+                Ok(false)
+            }
+        } else {
+            Err("Map has not enough lines for the required coordinates".to_owned())
+        }
+    }
+
+    /// Tells you if you can go to left for the provided coordinates. Coordinates MUST be normal coords (col/line).
+    // TODO: Remove allow dead code
+    #[allow(dead_code)]
+    pub fn can_go_to_sides_for_coords(&self, x: usize, y: usize) -> Result<(bool, bool), String> {
+        let content_coords = self.content_coords_for_coords(x, y)?;
+
+        Ok((
+            self.can_go_left_for_content_coords(content_coords.0, content_coords.1)?,
+            self.can_go_right_for_content_coords(content_coords.0, content_coords.1)?,
+        ))
+    }
 }
--- src/tests/map.rs
+++ src/tests/map.rs
@@ -2,83 +2,137 @@
 mod tests {
     use crate::map::Map;
 
-    #[test]
-    fn valid_params_generate_the_map() {
-        let map = Map::from_size_and_content(
-            4,
-            4,
-            vec![
-                "A B C".to_owned(),
-                "".to_owned(),
-                "".to_owned(),
-                "1 2 3".to_owned(),
-            ],
-        );
-
-        assert!(map.is_ok());
-    }
-
-    #[test]
-    fn invalid_height_does_not_create_a_map() {
-        let map = Map::from_size_and_content(
-            4,
-            4,
-            vec!["A B C".to_owned(), "".to_owned(), "1 2 3".to_owned()],
-        );
-
-        assert!(map.is_err());
-    }
-
-    #[test]
-    fn map_from_content_remove_entries_and_exits_from_content_and_size() {
-        let map = Map::from_size_and_content(
-            4,
-            4,
+    fn simple_map() -> Map {
+        Map::from_size_and_content(
+            5,
+            5,
             vec![
                 "A  B  C".to_owned(),
                 "|--|  |".to_owned(),
+                "|  |  |".to_owned(),
                 "|  |--|".to_owned(),
                 "1  2  3".to_owned(),
             ],
         )
-        .expect("Could not create map");
-
-        assert_eq!(map.height, 2);
-        assert_eq!(
-            map.content,
-            vec![
-                "|--||".chars().collect::<Vec<_>>(),
-                "||--|".chars().collect::<Vec<_>>()
-            ]
-        );
+        .expect("Could not create map")
     }
 
-    #[test]
-    fn invalid_height_is_not_validated() {
-        let map = Map {
-            width: 3,
-            height: 1,
-            entries: vec!["A".to_owned(), "B".to_owned(), "C".to_owned()],
-            exits: vec!["1".to_owned(), "2".to_owned(), "3".to_owned()],
-            content: vec![
-                "".chars().collect::<Vec<_>>(),
-                "".chars().collect::<Vec<_>>(),
-            ],
-        };
+    mod from_size_and_content {
+        use super::*;
 
-        assert!(map.validate().is_err());
+        #[test]
+        fn valid_params_generate_the_map() {
+            let map = Map::from_size_and_content(
+                4,
+                4,
+                vec![
+                    "A B C".to_owned(),
+                    "".to_owned(),
+                    "".to_owned(),
+                    "1 2 3".to_owned(),
+                ],
+            );
+
+            assert!(map.is_ok());
+        }
+
+        #[test]
+        fn invalid_height_does_not_create_a_map() {
+            let map = Map::from_size_and_content(
+                4,
+                4,
+                vec!["A B C".to_owned(), "".to_owned(), "1 2 3".to_owned()],
+            );
+
+            assert!(map.is_err());
+        }
+
+        #[test]
+        fn map_from_content_remove_entries_and_exits_from_content_and_size() {
+            let map = simple_map();
+
+            assert_eq!(map.height, 3);
+            assert_eq!(
+                map.content,
+                vec![
+                    "|--||".chars().collect::<Vec<_>>(),
+                    "|||".chars().collect::<Vec<_>>(),
+                    "||--|".chars().collect::<Vec<_>>()
+                ]
+            );
+        }
     }
 
-    #[test]
-    fn different_cout_of_entires_exits_is_not_validated() {
-        let map = Map {
-            width: 3,
-            height: 1,
-            entries: vec!["A".to_owned(), "B".to_owned(), "C".to_owned()],
-            exits: vec!["1".to_owned(), "2".to_owned()],
-            content: vec!["".chars().collect::<Vec<_>>()],
-        };
+    mod validate {
+        use super::*;
 
-        assert!(map.validate().is_err());
+        #[test]
+        fn invalid_height_is_not_validated() {
+            let map = Map {
+                width: 3,
+                height: 1,
+                entries: vec!["A".to_owned(), "B".to_owned(), "C".to_owned()],
+                exits: vec!["1".to_owned(), "2".to_owned(), "3".to_owned()],
+                content: vec![
+                    "".chars().collect::<Vec<_>>(),
+                    "".chars().collect::<Vec<_>>(),
+                ],
+            };
+
+            assert!(map.validate().is_err());
+        }
+
+        #[test]
+        fn different_cout_of_entires_exits_is_not_validated() {
+            let map = Map {
+                width: 3,
+                height: 1,
+                entries: vec!["A".to_owned(), "B".to_owned(), "C".to_owned()],
+                exits: vec!["1".to_owned(), "2".to_owned()],
+                content: vec!["".chars().collect::<Vec<_>>()],
+            };
+
+            assert!(map.validate().is_err());
+        }
+    }
+
+    mod content_coords_for_coords {
+        use super::*;
+
+        #[test]
+        fn content_coords_for_coords_returns_correct_coords() {
+            let map = simple_map();
+
+            assert_eq!(map.content_coords_for_coords(0, 0), Ok((0, 0)));
+            assert_eq!(map.content_coords_for_coords(2, 0), Ok((4, 0)));
+            assert_eq!(map.content_coords_for_coords(2, 1), Ok((2, 1)));
+            assert_eq!(map.content_coords_for_coords(1, 1), Ok((1, 1)));
+            assert_eq!(map.content_coords_for_coords(2, 2), Ok((4, 2)));
+        }
+    }
+
+    mod can_go_to_sides_for_coords {
+        use super::*;
+
+        #[test]
+        fn works_with_simple_cases() {
+            let map = simple_map();
+
+            assert_eq!(map.can_go_to_sides_for_coords(1, 0), Ok((true, false)));
+            assert_eq!(map.can_go_to_sides_for_coords(1, 1), Ok((false, false)));
+            assert_eq!(map.can_go_to_sides_for_coords(1, 2), Ok((false, true)));
+        }
+
+        #[test]
+        fn works_at_line_edges() {
+            let map = simple_map();
+
+            assert_eq!(map.can_go_to_sides_for_coords(0, 0), Ok((false, true)));
+            assert_eq!(map.can_go_to_sides_for_coords(2, 0), Ok((false, false)));
+            assert_eq!(map.can_go_to_sides_for_coords(0, 1), Ok((false, false)));
+            assert_eq!(map.can_go_to_sides_for_coords(2, 1), Ok((false, false)));
+            assert_eq!(map.can_go_to_sides_for_coords(0, 2), Ok((false, false)));
+            assert_eq!(map.can_go_to_sides_for_coords(2, 2), Ok((true, false)));
+        }
     }
 }
