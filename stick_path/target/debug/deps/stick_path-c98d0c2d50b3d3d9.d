e958377d8f96d460face373023310f2fe6b71a40
Thibaut Roche <thibaut.roche.perso@gmail.com>
2022-02-17 11:10:05 +0100

    [F] .gitignore & github actions file

--- stick_path/target/debug/deps/stick_path-c98d0c2d50b3d3d9.d
+++
@@ -1,8 +0,0 @@
-/home/thibaut/Projects/TechnicalTests/jellysmack_technical_test/stick_path/target/debug/deps/stick_path-c98d0c2d50b3d3d9: src/main.rs src/map.rs src/tests/mod.rs src/tests/map.rs
-
-/home/thibaut/Projects/TechnicalTests/jellysmack_technical_test/stick_path/target/debug/deps/stick_path-c98d0c2d50b3d3d9.d: src/main.rs src/map.rs src/tests/mod.rs src/tests/map.rs
-
-src/main.rs:
-src/map.rs:
-src/tests/mod.rs:
-src/tests/map.rs:
